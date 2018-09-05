c98853ef6704de923983d6f66ffde7c4c41188ac
Peter <merrygoat@users.noreply.github.com>
2018-09-05 14:45:24 +0100

    Tided plotting

--- fit.py
+++ fit.py
@@ -1,40 +1,57 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import plotting
 
 
-def main(do_plot_plane=1, do_plot__normal_vectors=1):
+def main(do_plot_plane=1, do_plot__normal_vectors=1, do_plot_raw_data=1):
     data = np.loadtxt("example_sheet.xyz", skiprows=2, usecols=(1, 2, 3))
 
     central_points = get_central_points(data)
-
+    neighbour_angles = []
+    vector_ax = plt.subplot(111, projection='3d')
     for selected_point_coordinate in central_points:
-        inverse_mask, neighbour_list = get_nearest_neighbours(data, selected_point_coordinate)
-        ax = plotting.plot_raw_data(data, central_points, neighbour_list, inverse_mask)
-        fit = do_plane_fit(neighbour_list)
+        inverse_mask, plane_neighbour_list = get_nearest_neighbours(data, selected_point_coordinate, [30, 30, 100])
+        fit = do_plane_fit(plane_neighbour_list)
         normal = get_plane_normal(selected_point_coordinate, fit)
+        _, bond_neighbour_list = get_nearest_neighbours(data, selected_point_coordinate, [20, 20, 20])
+        for particle in bond_neighbour_list:
+            if np.any(selected_point_coordinate != particle):
+                neighbour_vector = selected_point_coordinate - particle
+                neighbour_angles.append(angle_between(neighbour_vector, normal))
+        # Plotting
+        if do_plot_raw_data:
+            plotting.plot_raw_data(vector_ax, data, central_points, plane_neighbour_list, inverse_mask)
         if do_plot_plane:
-            plotting.plot_plane(ax, fit, selected_point_coordinate)
+            plotting.plot_plane(vector_ax, fit, selected_point_coordinate)
         if do_plot__normal_vectors:
-            plotting.plot_normal_vector(ax, selected_point_coordinate, normal)
-        if do_plot_plane or do_plot__normal_vectors:
-            plotting.set_equal_axes(ax, data)
+            plotting.plot_normal_vector(vector_ax, selected_point_coordinate, normal)
+        if do_plot_plane or do_plot_raw_data:
+            plotting.set_equal_axes(vector_ax, data)
             plt.show()
+        plotting.set_equal_axes(vector_ax, data)
+    plt.show()
+
+    plotting.plot_neighbour_angle(neighbour_angles)
 
 
 def get_central_points(data):
     """Fitting will be odd around the edges so select the points in the middle of the sheet"""
+    x_edge_cut = 0
+    y_edge_cut = 20
+    z_edge_cut = 10
     x = data[:, 0]
     y = data[:, 1]
     z = data[:, 2]
-    y_mask = np.logical_and(data[:, 1] > (y.min() + 50), data[:, 1] < (y.max() - 50))
-    z_mask = np.logical_and(data[:, 2] > (z.min() + 30), data[:, 2] < (z.max() - 30))
-    mask = np.logical_and(y_mask, z_mask)
+    x_mask = np.logical_and(data[:, 0] > (x.min() + x_edge_cut), data[:, 0] < (x.max() - x_edge_cut))
+    y_mask = np.logical_and(data[:, 1] > (y.min() + y_edge_cut), data[:, 1] < (y.max() - y_edge_cut))
+    z_mask = np.logical_and(data[:, 2] > (z.min() + z_edge_cut), data[:, 2] < (z.max() - z_edge_cut))
+    mask = np.logical_and.reduce(np.array([x_mask, y_mask, z_mask]))
     return data[mask, :]
 
 
 def get_plane_normal(start_point, fit):
-    """Calculate normal vector at one point on the plane"""
+    """Calculate normal vector of a plane described by the coefficients in fit"""
     point_1 = np.array([start_point[0],
                         start_point[1],
                         fit[0] * start_point[0] + fit[1] * start_point[1] + fit[2]])
@@ -51,11 +68,11 @@ def get_plane_normal(start_point, fit):
 
 
 def angle_between(v1, v2):
-    """ Returns the angle in radians between vectors 'v1' and 'v2' """
+    """ Returns the angle in degrees between vectors 'v1' and 'v2' """
     # First convert to unit vectors
     v1_u = v1 / np.linalg.norm(v1)
     v2_u = v2 / np.linalg.norm(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+    return np.degrees(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
 
 
 def do_plane_fit(local_points):
@@ -77,15 +94,15 @@ def do_plane_fit(local_points):
     return fit_coefficients
 
 
-def get_nearest_neighbours(data, selected_point_coordinate):
+def get_nearest_neighbours(data, selected_point_coordinate, distances):
     """ Given a point in the dataset, find its nearest neighbours """
 
     xdistances = np.abs(selected_point_coordinate[0] - data[:, 0])
     ydistances = np.abs(selected_point_coordinate[1] - data[:, 1])
     zdistances = np.abs(selected_point_coordinate[2] - data[:, 2])
-    xmask = xdistances < 30
-    ymask = ydistances < 30
-    zmask = zdistances < 100
+    xmask = xdistances < distances[0]
+    ymask = ydistances < distances[1]
+    zmask = zdistances < distances[2]
     distance_mask = np.logical_and(np.logical_and(xmask, ymask), zmask)
     inverse_mask = np.logical_not(distance_mask)
     local_points = data[distance_mask, :]
--- plotting.py
+++ plotting.py
@@ -1,6 +1,13 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+def plot_neighbour_angle(neighbour_angles):
+    np.savetxt("neighbour_angles.txt", neighbour_angles)
+    plt.hist(neighbour_angles, bins=np.arange(0, 190, 10), density=True)
+    plt.xlabel("Angle (degrees)")
+    plt.ylabel("Number of bonds/Total bonds")
+    plt.show()
 
 
 def plot_normal_vector(ax, start_point, normal):
@@ -13,7 +20,7 @@ def plot_plane(ax, fit, selected_point_coordinate):
     """ Plot a plane on the graph corresponding to the fit."""
     xlim = [selected_point_coordinate[0] - 50, selected_point_coordinate[0] + 50]
     ylim = [selected_point_coordinate[1] - 50, selected_point_coordinate[1] + 50]
-    x, y = np.meshgrid(np.arange(xlim[0], xlim[1]), np.arange(ylim[0], ylim[1]))
+    x, y = np.meshgrid(np.arange(xlim[0], xlim[1], 10), np.arange(ylim[0], ylim[1], 10))
     z = np.zeros(x.shape)
     for r in range(x.shape[0]):
         for c in range(x.shape[1]):
@@ -21,15 +28,12 @@ def plot_plane(ax, fit, selected_point_coordinate):
     ax.plot_wireframe(x, y, z, color='k')
 
 
-def plot_raw_data(data, central_points, local_points, inverse_mask):
+def plot_raw_data(ax, data, central_points, local_points, inverse_mask):
     # plot data points that are part of the fit in one colour and the rest in another colour
-    ax = plt.subplot(111, projection='3d')
-    ax.scatter(local_points[:, 0], local_points[:, 1], local_points[:, 2], color='b')
-    ax.scatter(central_points[:, 0], central_points[:, 1], central_points[:, 2], color='g')
-    ax.scatter(data[inverse_mask, 0], data[inverse_mask, 1], data[inverse_mask, 2], color='r')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    # ax.scatter(local_points[:, 0], local_points[:, 1], local_points[:, 2], color='b')
+    # ax.scatter(central_points[:, 0], central_points[:, 1], central_points[:, 2], color='g')
+    # ax.scatter(data[inverse_mask, 0], data[inverse_mask, 1], data[inverse_mask, 2], color='r', alpha=0.3)
+    ax.scatter(data[:, 0], data[:, 1], data[:, 2], color='r', alpha=0.5)
 
     return ax
 
@@ -47,3 +51,6 @@ def set_equal_axes(ax, data):
     ax.set_xlim(mid_x - max_range, mid_x + max_range)
     ax.set_ylim(mid_y - max_range, mid_y + max_range)
     ax.set_zlim(mid_z - max_range, mid_z + max_range)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
